8d302a8e43a19409acd4a8f0cb01d71811ff5dc4
wsqleedcode <459060169@qq.com>
2020-02-25 11:12:40 -0500

    leetcode

---
+++ leetcode206.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: leetcode206.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月25日 星期二 11时10分04秒
+ ************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+  struct ListNode ret, *p = head, *q;
+    ret.next = NULL;
+    while (p) {
+        q = p->next;
+        p->next = ret.next;
+        ret.next = p;
+        p = q;
+    }
+    return ret.next;
+}
