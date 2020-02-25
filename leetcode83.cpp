2b27d6b3fb33fb4da07d710578ed776d44c573e7
wsqleedcode <459060169@qq.com>
2020-02-25 11:08:10 -0500

    leetcode83

---
+++ leetcode83.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: leetcode83.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月25日 星期二 11时06分17秒
+ ************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    struct ListNode *p = head, *q;
+    while(p && p->next){
+        if(p->val != p->next->val){
+            p = p->next;
+        }else{
+            q = p->next;
+            p->next = q->next;
+            free(q);
+        }
+    }return head;
+}
